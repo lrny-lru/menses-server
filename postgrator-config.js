2266004f00b63084d292fd2c4c287e1b1370413c
lrny-lru <60946728+lrny-lru@users.noreply.github.com>
2020-12-18 16:33:13 -0500

    debugging POST

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory': 'migrations',
   'driver': 'pg',
-  'connectionString': process.env.TEST_DB_URL ,
+  'connectionString': process.env.DATABASE_URL  + '?ssl=true' ,
 };
 
 
